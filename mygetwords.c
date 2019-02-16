018bc4e87dcffdf8a756d0431f7e10a9c7583f25
MasahiroKiji <mkiji@coe.edu>
2019-02-15 19:38:28 -0600

    Create files and make getwords function

---
+++ mygetwords.c
@@ -0,0 +1,19 @@
+#include "mygetwords.h"
+
+int getwords(char* str){
+
+  int word_count = 0;
+  int index = 0;
+  while (str[index] != '\n'){
+
+    while (myisspace(str[index]) == 0)
+    {
+      index++;
+    }
+
+    word_count++;
+  }
+
+  return word_count;
+
+}
