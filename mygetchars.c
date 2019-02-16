7d9924ea7bacbf4da7806647dabd8083b4337908
MasahiroKiji <mkiji@coe.edu>
2019-02-15 19:44:02 -0600

    Write getchar function and prototype

--- mygetchars.c
+++ mygetchars.c
@@ -1,1 +1,14 @@
+#include "mygetchar"
 
+int getchars(char* str){
+
+  int index = 0;
+  int char_count = 0;
+
+  while(str[index] != '\n'){
+    if (myisspace(str[index]) == 0)
+      char_count++
+    index++;
+  }
+  return char_count;
+}
